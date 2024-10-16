2ebed49a688f5778cbf82064e686b031630c28d3
jay <emma.jordan@bsu.edu>
2024-10-16 18:34:05 -0400

    editing of text

---
+++ project2/src/app/flashcards/page.tsx
@@ -0,0 +1,11 @@
+
+'use client'
+
+export default function Flashcards() {
+
+  return (
+    <>
+    <text>This is the Flashcards page. This will allow you to play Flashcards.</text>
+    </>
+  );
+}
