5d9b3260e9f862aa83716c5083a0f182088307d5
jay <emma.jordan@bsu.edu>
2024-10-23 17:15:06 -0400

    editing of card information

--- project2/seed.js
+++ project2/seed.js
@@ -4,9 +4,9 @@ const prisma = new PrismaClient()
 const seed = async () => {
     const createMany = await prisma.cards.createMany({
         data: [
-            {prompt: 'Cat Video', answer: '/videos/test.mp4', correct: 10, time: 100},
-            {prompt: 'Dog Video', answer: '/videos/test.mp4', correct: 10, time: 100},
-            {prompt: 'Analysis Video', answer: '/videos/test.mp4', correct: 10, time: 100},
+            {prompt: '', answer: '', correct: 1, time: 2},
+            {prompt: '', answer: '', correct: 1, time: 2},
+            {prompt: '', answer: '', correct: 1, time: 2},
         ]
     })
 }
